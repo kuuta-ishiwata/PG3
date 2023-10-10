90011ee9b83bb6194843f0e861ea11ad631af683
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2023-10-10 10:12:22 +0900

    プロジェクト ファイルを追加します。

---
+++ PG3/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+template <typename T>
+
+T Min(T a, T b)
+{
+
+	if (a > b)
+	{
+
+		return  static_cast<float>(a);
+
+	}
+
+	else
+	{
+
+		return static_cast<float>(b);
+
+	};
+
+}
+
+template <>
+char Min<char>(char a, char b)
+{
+	return printf("”šˆÈŠO‚Í“ü—Í‚Å‚«‚Ü‚¹‚ñ\n");
+
+}
+
+
+int main(void)
+{
+
+
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(11.4f, 51.4f));
+	printf("%f\n", Min<double>(11.4f, 51.4f));
+	
+	Min<char>('a', 'b');
+
+	return 0;
+
+}
+
+
+
